30e98633e359cc8784b56b0d235e38b82dc21cb4
HL <rlimm2@knou.ac.kr>
2024-05-27 18:16:11 +0900

    5-27-1

--- 3.PYTHON/10. project/my_project/generator4.py
+++ 3.PYTHON/10. project/my_project/generator4.py
@@ -28,27 +28,31 @@ class DataGenerator:
             address = self.address_gen.generate_address()
 
             self.data.append((id, name, birthdate, gender, address))  # append()는 인자가 하나여야 함... 튜플로 한번에 넣기
-            #a_user = (id, name, birthdate, gender, address)
-            #data.apend(a_user)
+            # a_user = (id, name, birthdate, gender, address)
+            # self.data.apend(a_user)
 
 
 # 메인 함수
 if __name__ == "__main__":
+    my_printer = DataPrinter()
+
     if len(sys.argv) <= 1:  #사용자 입력 인자가 없으면..
         num_data = input("생성을 원하는 데이터 개수를 입력하세요: ")
     else:                   # 사용자 입력 인자가 있으면...
         num_data = sys.argv[1]
+        
+    users1 = DataGenerator(int(num_data))
+    users1.generate_users()     # 실행할 때마다 새로 생성
+        
+    my_printer.print_to_screen(users1.data)
+
 
     # argv[0] = 실행파일명, arvc[1] = 실제 첫번째 인자값
     # print("입력 인자 확인: 첫 번째 인자: {}, 두 번째 인자: {}".format(sys.argv[0],sys.argv[1])
 
     # num_data = input('생성을 원하는 데이터 개수를 입력하세요: ')
 
-    users1 = DataGenerator(int(num_data))
-    users1.generate_users()     # 실행할 때마다 새로 생성
-
     # 우리가 원하는 데이터 출력 - 화면, 파일
-    my_printer = DataPrinter()
     if len(sys.argv) == 3:
         if sys.argv[2] == 'screen':
             my_printer.print_to_screen(users1.data)
--- 3.PYTHON/10. project/my_project/printers/output_printer.py
+++ 3.PYTHON/10. project/my_project/printers/output_printer.py
@@ -6,7 +6,7 @@ class DataPrinter:
             print(d)
 
     def print_to_file(self, data):
-        with open('output.txt', 'w') as file:
+        with open('output.txt', 'w', encoding= 'utf -8') as file:
             writer = csv.writer(file)
             for d in data:
                 writer.writerow(d)
