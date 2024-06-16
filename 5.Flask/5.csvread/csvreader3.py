3c6abf7127820d7a0afeca260548eec8380fed5f
HL <rlimm2@knou.ac.kr>
2024-06-16 23:24:49 +0900

    6/16

--- 5.Flask/5.csvread/csvreader3.py
+++ 5.Flask/5.csvread/csvreader3.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/<int:page>")
 def search1(page=1):
-    per_page = int(request.args.get('per_page', default=10))    # 한 페이지에 보여줄 항목 수
+    per_page = 10    # 한 페이지에 보여줄 항목 수
     
     start_index = (page - 1) * per_page
     end_index = page * per_page
@@ -64,15 +64,15 @@ def search2(page=1):
     return render_template('index3.html', headers=headers, users=index_pages, total_pages=total_pages, per_page = per_page)
 
 
-@app.route("/user/id")
+@app.route("/user/<id>")
 def user_detail(id):
-    per_page = int(request.args.get('per_page', default=10))
-
+    headers = csv_data[0]
+    print(id)
     for u in csv_data:
         if u['Id'] == id:
-            user = u['Id']
-
-    return render_template('user_detail.html', headers=headers, user=user, per_page = per_page)
+            user = u
+    print(user)
+    return render_template('user_detail.html', headers=headers, user=user)
 
 if __name__ =='__main__':
     load_csv_data('./users.csv')                # 여기에서 로딩해야 한 번만 하고 끝남.
