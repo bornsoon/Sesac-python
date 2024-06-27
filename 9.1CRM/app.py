e9158e4bee9d7e0bfcd7bcecd8b1751ff4d37571
HL <rlimm2@knou.ac.kr>
2024-06-27 18:02:18 +0900

    6/27

--- 9.1CRM/app.py
+++ 9.1CRM/app.py
@@ -1,16 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 import db as db
+import math
 
 app = Flask(__name__)
 
+def pagings(category, page):
+    total_page = math.ceil(len(category) / 20)
+    prePage = True if page < 8 else False
+    nextPage = True if page < (total_page - 6) else False
+
+    return {"category": 'user', "total": total_page, "pre": prePage, "next": nextPage }
+
+
 @app.route('/')
+def home():
+    return redirect(url_for('user'))
+
+
 @app.route('/user')
-@app.route('/user/<page>')
-def user():
-    query = "SELECT * FROM users"
-    values = db.get_query(query)
-    keys = values[0].keys()
-    return render_template('user.html', keys=keys, values=values)
+@app.route('/user/<int:page>')
+def user(page=1):
+    name = request.form.get('name')
+    gender = request.form.get('gender')
+    
+    query = "SELECT id, name, gender, age, birthdate FROM users"
+
+    if name or gender:
+        query= "SELECT * FROM users WHERE name LIKE '%name%'"
+
+    user = db.get_query(query)
+    keys = user[0].keys()
+    firstIndex = (page - 1) * 20
+    values = user[firstIndex : firstIndex+20]
+    
+    paging = pagings(user, page)
+
+    return render_template('user.html', keys=keys, values=values, page=page, paging=paging)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
