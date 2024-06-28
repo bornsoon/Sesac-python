e50479af40a34b53f722926b29950ba508e7b116
HL <rlimm2@knou.ac.kr>
2024-06-28 11:45:45 +0900

    6/28

--- 9.1CRM/app.py
+++ 9.1CRM/app.py
@@ -4,8 +4,9 @@ import math
 
 app = Flask(__name__)
 
-def pagings(category, page):
-    total_page = math.ceil(len(category) / 20)
+
+def pagings(category, page, per_page):
+    total_page = math.ceil(len(category) / per_page)
     prePage = True if page < 8 else False
     nextPage = True if page < (total_page - 6) else False
 
@@ -17,26 +18,50 @@ def home():
     return redirect(url_for('user'))
 
 
-@app.route('/user')
-@app.route('/user/<int:page>')
+@app.route('/user', methods=['GET', 'POST'])
+@app.route('/user/<int:page>', methods=['GET', 'POST'])
 def user(page=1):
-    name = request.form.get('name')
-    gender = request.form.get('gender')
+    query= "SELECT id, name, gender, age, birthdate FROM users LIMIT 1"
+    user1 = db.get_query(query)
+    keys = user1[0].keys()
+
     
-    query = "SELECT id, name, gender, age, birthdate FROM users"
+    if request.method == 'POST':
+        per_page = int(request.form['per_page'])
+        name = request.form['name']
+        gender = request.form['gender']
+    elif request.method == "GET":
+        per_page = int(request.args.get('per_page', default=20))
+        name = request.args.get('name')
+        gender = request.args.get('gender')
 
-    if name or gender:
-        query= "SELECT * FROM users WHERE name LIKE '%name%'"
+    if gender:
+        query= "SELECT id, name, gender, age, birthdate FROM users WHERE name LIKE ?||'%' AND gender = ?"
+        params = (name, gender)
+    else:
+        if not name:
+            name = ""
+        query= "SELECT id, name, gender, age, birthdate FROM users WHERE name LIKE ?||'%'"
+        params = (name, )
 
-    user = db.get_query(query)
-    keys = user[0].keys()
-    firstIndex = (page - 1) * 20
-    values = user[firstIndex : firstIndex+20]
+    user = db.get_query(query, params)
+
+    firstIndex = (page - 1) * per_page
+    values = user[firstIndex : firstIndex+per_page]
     
-    paging = pagings(user, page)
+    paging = pagings(user, page, per_page)
 
-    return render_template('user.html', keys=keys, values=values, page=page, paging=paging)
+    return render_template('user.html', keys=keys, values=values, page=page, per_page=per_page, paging=paging, name=name, gender=gender)
 
+
+@app.route('/order')
+def order():
+    query= "SELECT id, name, gender, age, birthdate FROM order"
+    data = db.get_query(query)
+    keys = data[0].keys()
+    
+
+    return render_template('table.html', keys=keys, values=values, page=page, paging=paging, name=name, gender=gender)
 
 if __name__ == '__main__':
     app.run(debug=True)
