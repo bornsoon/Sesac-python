d97330d47bbb186c1f7ebcb0e2fa93364443cc0b
HL <rlimm2@knou.ac.kr>
2024-06-29 19:43:47 +0900

    6-/29

--- 9.1CRM/app.py
+++ 9.1CRM/app.py
@@ -60,13 +60,13 @@ def userDetail(id):
     user = db.get_query(user_query, (id,))
     keys = user[0].keys()
 
-    order_query = "SELECT i.item, o.orderAt, s.name FROM orders o JOIN stores s ON o.storeId=s.Id JOIN orderItems oi ON o.id=oi.orderId JOIN items i ON oi.itemId=i.id WHERE o.userId = ? ORDER BY o.orderAt"
+    order_query = "SELECT o.id, o.orderAt, s.name FROM orders o JOIN stores s ON o.storeId=s.Id WHERE o.userId = ? ORDER BY o.orderAt DESC"
     orders = db.get_query(order_query, (id,))
 
-    store_query = "SELECT s.name FROM orders o INNER JOIN stores s ON o.storeId=s.Id WHERE o.userId = ? GROUP BY s.Id ORDER BY count(s.Id) DESC LIMIT 5"
+    store_query = "SELECT s.name, count(s.id) AS 'count' FROM orders o INNER JOIN stores s ON o.storeId=s.Id WHERE o.userId = ? GROUP BY s.Id ORDER BY count(s.Id) DESC LIMIT 5"
     topStores = db.get_query(store_query, (id,))
 
-    item_query = "SELECT i.item FROM items i INNER JOIN orderItems oi ON i.Id=oi.itemId INNER JOIN orders o ON oi.orderId=o.id WHERE o.userId = ? GROUP BY i.Id ORDER BY count(i.id) DESC LIMIT 5"
+    item_query = "SELECT i.item, count(i.item) AS 'count' FROM items i INNER JOIN orderItems oi ON i.Id=oi.itemId INNER JOIN orders o ON oi.orderId=o.id WHERE o.userId = ? GROUP BY i.Id ORDER BY count(i.id) DESC LIMIT 5"
     topItems = db.get_query(item_query, (id,))
 
     return render_template('userDetail.html', keys=keys, paging=paging, user=user[0], orders=orders, topStores=topStores, topItems=topItems)
@@ -89,6 +89,7 @@ def order(page=1):
     paging = pagings(order, 'order', page, per_page)
 
     return render_template('order.html', keys=keys, values=values, page=page, per_page=per_page, paging=paging)
+
 
 
 @app.route('/orderItem', methods=['GET', 'POST'])
@@ -146,6 +147,24 @@ def store(page=1):
 
     return render_template('store.html', keys=keys, values=values, page=page, per_page=per_page, paging=paging)
 
+
+@app.route('/storeDetail/<id>')
+def storeDetail(id):
+    paging = {'category': "store"}
+    store_query= "SELECT * FROM stores WHERE id = ?"
+    store = db.get_query(store_query, (id,))
+    keys = store[0].keys()
+
+    order_query = "SELECT o.id, o.orderAt, s.name, o.storeId FROM orders o JOIN stores s ON o.storeId=s.Id WHERE o.userId = ? ORDER BY o.orderAt DESC"
+    orders = db.get_query(order_query, (id,))
+
+    store_query = "SELECT s.name, count(s.id) AS 'count' FROM orders o INNER JOIN stores s ON o.storeId=s.Id WHERE o.userId = ? GROUP BY s.Id ORDER BY count(s.Id) DESC LIMIT 5"
+    topStores = db.get_query(store_query, (id,))
+
+    item_query = "SELECT i.item, count(i.item) AS 'count' FROM items i INNER JOIN orderItems oi ON i.Id=oi.itemId INNER JOIN orders o ON oi.orderId=o.id WHERE o.userId = ? GROUP BY i.Id ORDER BY count(i.id) DESC LIMIT 5"
+    topItems = db.get_query(item_query, (id,))
+
+    return render_template('storeDetail.html', keys=keys, paging=paging, store=store[0], orders=orders, topStores=topStores, topItems=topItems)
 
 if __name__ == '__main__':
     app.run(debug=True)
